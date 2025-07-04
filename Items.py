ee8c82da0de42093cfb2686f2550e6538e05ac0b
Noah Weeks <115123563+Nullgrimoire@users.noreply.github.com>
2025-07-03 23:24:19 -0700

    Create Items.py

---
+++ Items.py
@@ -0,0 +1,8 @@
+ITEMS = {
+    "Wyvern Scales": {"base_price": 50, "volatility": 0.3},
+    "Ectoplasm": {"base_price": 70, "volatility": 0.5},
+    "Elven Wine": {"base_price": 40, "volatility": 0.2},
+    "Phoenix Feather": {"base_price": 120, "volatility": 0.6},
+    "Goblin Tech": {"base_price": 90, "volatility": 0.4},
+    "Dragonbone": {"base_price": 200, "volatility": 0.7},
+}
